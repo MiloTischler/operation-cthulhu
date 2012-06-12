d487e3c35c3e80d562ab838f016d0f82e38f0dc5
MiloTischler <milo.tischler@gmail.com>
2012-06-12 20:41:48 +0200

    Added mongodb

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
+var express = require('express');
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 
-var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
 
 var app = module.exports = express.createServer();
 
@@ -15,7 +13,6 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.logger());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
@@ -24,24 +21,25 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+  app.use(express.errorHandler()); 
 });
 
+var articleProvider = new ArticleProvider('localhost', 27017);
 // Routes
-var articleProvider = new ArticleProvider();
 
 app.get('/', function(req, res){
-  articleProvider.findAll(function(error, docs){
-      res.render('index.jade', { locals: {
-          title: 'Blog',
-          articles: docs
-        }
-      });
-  });
+    articleProvider.findAll( function(error,docs){
+        res.render('index.jade', { 
+            locals: {
+                title: 'Blog',
+                articles:docs
+            }
+        });
+    })
 });
 
 app.get('/new', function(req, res) {
@@ -60,7 +58,26 @@ app.post('/new', function(req, res){
     });
 });
 
-
-app.listen(8888, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.get('/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show.jade',
+        { locals: {
+            title: article.title,
+            article:article
+        }
+        });
+    });
 });
+
+app.post('/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+});
+
+app.listen(8888);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
