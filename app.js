443086a36b2c3d189eeb09a0fcb861cf71e9ee76
MiloTischler <milo.tischler@gmail.com>
2012-06-12 19:49:19 +0200

    Added some features

--- app.js
+++ app.js
@@ -6,6 +6,8 @@
 var express = require('express')
   , routes = require('./routes');
 
+var ArticleProvider = require('./articleprovider-memory').ArticleProvider;
+
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -13,6 +15,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.logger());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
@@ -29,9 +32,35 @@ app.configure('production', function(){
 });
 
 // Routes
+var articleProvider = new ArticleProvider();
 
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  articleProvider.findAll(function(error, docs){
+      res.render('index.jade', { locals: {
+          title: 'Blog',
+          articles: docs
+        }
+      });
+  });
+});
 
-app.listen(3000, function(){
+app.get('/new', function(req, res) {
+    res.render('blog_new.jade', { locals: {
+        title: 'New Post'
+    }
+    });
+});
+
+app.post('/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+
+app.listen(8888, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
