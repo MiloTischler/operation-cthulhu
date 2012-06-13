cca65e9a0586dcf0c085dd13a86d887fee04f938
Jakob Schweighofer <theschweigi@gmail.com>
2012-06-13 14:42:11 +0200

    refined registration

--- routes/register.js
+++ routes/register.js
@@ -35,5 +35,8 @@ module.exports = function(app, models) {
 
             console.log('New user saved.');
         });
+
+        req.flash('info', 'Registration successful!');
+        res.redirect('/register');
     });
 }
