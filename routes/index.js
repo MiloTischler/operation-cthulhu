0e1c5125b1705421a80cf4d52523ef04306e010c
Jakob Schweighofer <theschweigi@gmail.com>
2012-06-13 12:16:59 +0200

    User registration implemented

--- routes/index.js
+++ routes/index.js
@@ -5,52 +5,6 @@ module.exports = function(app, models) {
         res.redirect('/posts');
     });
 
-    // load login page
-    app.get('/login', function(req, res) {
-        res.render('login.jade', {
-            title: 'Login'
-        });
-    });
-
-    // login handler
-    app.post('/login', function(req, res) {
-        var loginName = req.param('loginName', null);
-        var password = req.param('password', null);
-
-        console.log("user: " + loginName + " pw: " + password);
-    });
-
-    // load registration page
-    app.get('/register', function(req, res) {
-        res.render('register.jade', {
-            title: 'Registration'
-        });
-    });
-
-    // registration handler
-    app.post('/register', function(req, res) {
-
-        var loginName = req.param('loginName', null);
-        var password = req.param('password', null);
-        var passwordRepeat = req.param('passwordRepeat', null);
-
-        if (password != passwordRepeat) {
-            console.log("Flash should appear!");
-            req.flash('info', 'Passwords must be the same!');
-            res.redirect('/register');
-        }
-
-        var User = models.users;
-        var registeredUser = new User();
-        registeredUser.name = loginName;
-        registeredUser.password = password;
-
-        // now kiss
-        console.log("user: " + loginName + " pw: " + password + " pwr: " + passwordRepeat);
-        registeredUser.save();
-    });
-
-
     // load userlist page
     app.get('/userList', function(req, res) {
         res.render('userList.jade', {
@@ -60,6 +14,4 @@ module.exports = function(app, models) {
             user3: 'Jakob'
         });
     });
-
-
 }
