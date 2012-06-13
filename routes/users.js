35fee4607d826adea3d627d6626b6f7adebd38b2
Neuhold Paul <paul.neuhold@student.tugraz.at>
2012-06-13 13:09:36 +0200

    muahaha

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,39 @@
 module.exports = function(app, models) {
 
-   // load userlist page
-    app.get('/userList', function(req, res) {
+   // // load userlist page
+   //  app.get('/userList', function(req, res) {
         
-       // var User = models.users;
-        res.render('userList.jade', {
-            title: 'Userlist'
-        //    users: users
-        });
-    });
+   //      var User = models.users;
 
-    // handle userlist page
-    app.post('/userList', function(req, res) {
+   //      User.find({}).asc('name').run(function(err, users) {
+   //          if (err) throw err;
 
-        var User = models.user;
+   //          res.render('userList.jade', {
+   //              title: 'schweigi',
+   //              users: users
+
+   //          });
+   //          console.log('user:');
+   //      });
+   //       console.log('user:2');
+   //  });
+
+    // // handle userlist page
+    // app.post('/userList', function(req, res) {
+
+    //     var User = models.user;
    
-        User.find({}).desc('username').run(function(err, user) {
-            if (err) throw err;
+    //     User.find({}).desc('loginName').run(function(err, user) {
+    //         if (err) throw err;
 
-            res.render('userlist.jade', {
-                title: 'Userlist',
-                user: user
-            });
+    //         res.render('userlist.jade', {
+    //             title: 'Userlist',
+    //             users: users
+    //         });
 
-            console.log('Loaded Users:');
-            console.log(user);
-        });
-    });
+    //         console.log('Loaded Users:');
+    //         console.log(users);
+    //     });
+    // });
 
 }
